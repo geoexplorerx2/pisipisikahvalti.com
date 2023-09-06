24fc48507a85636ecac37ca5ed0db46172b55b45
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-09-06 03:36:32 -0700

    master

--- resources/views/__menu_page.blade.php
+++ resources/views/__menu_page.blade.php
@@ -10,9 +10,7 @@
     <div class="relative w-full min-h-screen max-w-[540px]">
         <div class="relative z-0 w-full">
             <div class="w-full px-5 py-3">
-                <audio class="w-full" src="{{ asset('public/uploads/'.$__Music[0]->music) }}" controls>
-                    <p>If you are reading this, it is because your browser does not support the audio element.</p>
-                </audio>
+                
             </div>
         </div>
         <div class="relative z-0 w-full">
